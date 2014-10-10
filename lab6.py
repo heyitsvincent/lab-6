b8f4e15e6e4c8adab1647ba071caa90bd6b31542
heyitsvincent <vincent@1-pps.com>
2014-10-10 14:48:00 -0500

    lab 6

---
+++ lab6.py
@@ -0,0 +1,21 @@
+total=0
+print "how many are you adding"
+userinput= raw_input()
+for x in range(userinput):
+    print "what is the number"
+    total = total + userinput 
+    print total 
+    
+total2 =[]
+for x in range(3):
+    print "input a number"
+    userinput = int(raw_input())
+    total2.append(userinput)
+print sum(total2)
+
+total3=1
+userinput = int(raw_input())
+for x in range((1, userinput+1, 1)):
+    total=total * x 
+print total3
+
